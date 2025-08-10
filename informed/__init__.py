1b3acf56961209ab1d48df280676cf4ef75861ce
Sam Sifiso Mndebele <sams.mndebele@gmail.com>
2025-08-10 20:45:15 +0200

    Refactor code structure by splitting search strategy demos into respective modules

---
+++ informed/__init__.py
@@ -0,0 +1,34 @@
+from informed.best_first_search import best_first_search, visited_order, found_path
+
+graph = {
+    0: [4, 1, 16],
+    4: [9, 13],
+    13: [10, 12],
+    9: [],
+    10: [],
+    12: [],
+    1: [2, 5, 7],
+    2: [],
+    5: [11, 14],
+    11: [],
+    14: [],
+    7: [],
+    16: [15, 8],
+    15: [],  # Goal
+    8: [17, 6, 3],
+    17: [],
+    6: [],
+    3: []
+}
+
+# Heuristic: assumes nodes are integers; closer numbers are "closer" to the goal
+def heuristic(n: int, goal: int) -> float:
+    return abs(n - goal)
+
+print("Best First Search")
+if best_first_search(graph, 0, 15, heuristic):
+    print("Visited order:", visited_order)
+    print("Path to goal:", found_path)
+else:
+    print("Goal not found")
+print()
---
+++ uninformed/__init__.py
@@ -0,0 +1,40 @@
+from uninformed.breadth_first_search import breadth_first_search, visited_order as bfs_visited_order, found_path as bfs_found_path
+from uninformed.depth_first_search import depth_first_search, visited_order as dfs_visited_order, found_path as dfs_found_path
+
+graph = {
+    0: [4, 1, 16],
+    4: [9, 13],
+    13: [10, 12],
+    9: [],
+    10: [],
+    12: [],
+    1: [2, 5, 7],
+    2: [],
+    5: [11, 14],
+    11: [],
+    14: [],
+    7: [],
+    16: [15, 8],
+    15: [],  # Goal
+    8: [17, 6, 3],
+    17: [],
+    6: [],
+    3: []
+}
+
+# Run Backtracking / DFS to find 15
+print("Depth First Search")
+if depth_first_search(graph, 0, goal = 15):
+    print("Visited order:", dfs_visited_order)
+    print("Path to goal:", dfs_found_path)
+else:
+    print("Goal not found")
+print()
+
+print("Breadth First Search")
+if breadth_first_search(graph, 0, goal = 15):
+    print("Visited order:", bfs_visited_order)
+    print("Path to goal:", bfs_found_path)
+else:
+    print("Goal not found")
+print()
